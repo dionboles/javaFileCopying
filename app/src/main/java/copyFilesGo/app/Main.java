16d49764e3d9e2d1f4ac5832c2f929e0ed6e116b
EK <86498073+ekdeveloper@users.noreply.github.com>
2022-12-20 14:20:31 -0500

    Update Main.java
    food for thought

--- app/src/main/java/copyFilesGo/app/Main.java
+++ app/src/main/java/copyFilesGo/app/Main.java
@@ -7,45 +7,53 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-
-
 import javax.print.DocFlavor;
 import javax.swing.*;
 
 import org.apache.commons.io.FileUtils;
 
+/**
+Description of class
+**/
 class Filewalker {
 
-    public void CopyDirectoryRecursively(String sourceDir, String destinationDir) {
+    /**
+    Description of method
+    @param arg description of parameter
+    **/
+    public void CopyDirectoryRecursively(String sourcePath, String targetPath) throw Exception {
 
-        String sourcePath = sourceDir;
-        String targetPath = destinationDir;
         System.out.printf("Copying %s to %s%n", sourcePath, targetPath);
+        
         File sourcFile = new File(sourcePath);
-        File targetFile = new File(targetPath);
-        FileFilter
-        try {
-            FileUtils.copyDirectory(sourcFile, targetFile);
-        } catch (Exception e) {
-            System.out.println(e);
-        }
+        File targetFile = new File(targetPath);        
+
+        FileUtils.copyDirectory(sourcFile, targetFile);
     }
 
 }
     /**
-     * main
+     * Description of class
      */
     public class Main {
-
+        
+        static Filewalker file_walker = new Filewalker();
+        
+        /**
+        Description of method
+        @param arg description of parameter
+        **/
         public static void main(String[] args) {
             String sourceDir = Paths.get("/Users/dionboles/Documents").toString();
             String destinationDir = Paths.get(
                     "/Users/dionboles/Library/Containers/com.wdc.WDDesktop.WDDesktopFinderSync/Data/volumes/e161eba9-2a6b-4d7c-af87-31f627c83d7f/Dion's My Cloud Home/Coding").toString();
             try {
-                Filewalker file_walker = new Filewalker();
-                file_walker.CopyDirectoryRecursively(sourceDir, destinationDir);
+                //Filewalker file_walker = new Filewalker();
+                Main.file_walker.CopyDirectoryRecursively(sourceDir, destinationDir);
+                
+                //Catch all exception here
             } catch (Exception e) {
-                System.out.println(e.toString());
+                System.out.println("Exception: " + e.message(), e);
             }
         }
     }
